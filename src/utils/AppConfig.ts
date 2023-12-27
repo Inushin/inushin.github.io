aadee08edf31a85b8311c2371604739bebe9ee2b
Aitor Sobera <aitorsobera@gmail.com>
2023-12-27 19:33:45 +0100

    feat(AppConfig.ts): update for custom config

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
-  locale_region: 'en-us',
-  locale: 'en',
+  site_name: "Inushin's blog",
+  title: "Inushin's blog",
+  description: "Inushin's blog built with Astro using React and Tailwind CSS",
+  author: 'Aitor',
+  locale_region: 'es-es',
+  locale: 'es',
 };
